1a367d6970f86c495eab6d618c9b36de57b5b241
nicollasOR <nicollas.oliveira.rufino30@gmail.com>
2026-08-10 17:25:49 -0300

    :sparkles: feat: start UsuarioService

--- AgendaCampo/Applications/Services/UsuarioService.cs
+++ AgendaCampo/Applications/Services/UsuarioService.cs
@@ -1,6 +1,50 @@
-namespace AgendaCampo.Applications.Services;
+using AgendaCampo.DTOs.UsuarioDTO;
+using AgendaCampo.Domains;
+using AgendaCampo.Exceptions;
+using AgendaCampo.Interface;
+using System.Runtime.Intrinsics.Arm;
+using System.Security.Cryptography;
+using System.Text;
 
-public class UsuarioService
+namespace RoyalGamess.Aplications.Services
 {
-    
+    public class UsuarioService
+    {
+
+        private readonly IUsuarioRepository _rep;
+        
+        public UsuarioService(IUsuarioRepository repo) => _rep = repo;
+
+        private static byte[] HashSenha_(string senha)
+        {
+            if (string.IsNullOrEmpty(senha)) 
+                throw new DomainException("Senha eh obrigatoria"); 
+            using var sha256 = SHA256.Create();
+            return sha256.ComputeHash(Encoding.UTF8.GetBytes(senha));
+        }
+        
+        private static lerUsuarioDTO lerDTO(Usuario usuario)
+        {
+            lerUsuarioDTO usuarioDTO = new lerUsuarioDTO
+            {
+                usuarioID = usuario.usuarioID,
+                email = usuario.email,
+                nome = usuario.nome,
+                // senha = 
+                statusUsuario = usuario.statusUsuario ?? true
+            };
+
+            return usuarioDTO;
+        }
+        
+        public List<lerUsuarioDTO> Listar()
+        {
+            List<Usuario> Listar = _rep.Listar();
+
+            List<lerUsuarioDTO> lerUsuarioDTO = Listar.Select(varAux => lerDTO(varAux)).ToList();
+            return lerUsuarioDTO;
+        }
+
+    }
+
 }
