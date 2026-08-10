5c3fe4655ed223cb6390d4276298d3ef0caeebc9
nicollasOR <nicollas.oliveira.rufino30@gmail.com>
2026-08-10 17:26:27 -0300

    :sparkles: feat: start UsuarioController

--- AgendaCampo/Controllers/UsuarioController.cs
+++ AgendaCampo/Controllers/UsuarioController.cs
@@ -1,6 +1,37 @@
-namespace AgendaCampo.Controllers;
+using AgendaCampo.DTOs.UsuarioDTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RoyalGamess.Aplications.Services;
 
-public class UsuarioController
+// using AgendaCampo.Apl
+
+namespace AgendaCampo.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly UsuarioService _service;
+
+        public UsuarioController(UsuarioService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public ActionResult<List<lerUsuarioDTO>> Listar()
+        {
+            List<lerUsuarioDTO> list = _service.Listar();
+            if (list == null)
+            {
+                return NotFound(list);
+            }
+            else
+            {
+                return Ok(list);
+            }
+
+        }
+    }
     
 }
