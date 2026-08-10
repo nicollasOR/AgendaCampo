bb32cfc9a29e73d86c6fd3743c8969929c6c92db
nicollasOR <nicollas.oliveira.rufino30@gmail.com>
2026-08-10 15:21:11 -0300

    :sparkles: feat: config program and add .env.example

--- AgendaCampo/Program.cs
+++ AgendaCampo/Program.cs
@@ -1,21 +1,46 @@
+using DotNetEnv;
+
+Env.Load();
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+
+// cors btw
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 
+app.UseCors("CorsPolicy");
+
+// ATENÇÃO: Autenticação SEMPRE ANTES da Autorização
+app.UseAuthentication(); 
 app.UseAuthorization();
 
 app.MapControllers();
