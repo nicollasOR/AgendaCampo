d4b366cc27299ee0c6eb2ab490a6bb38a5406460
nicollasOR <nicollas.oliveira.rufino30@gmail.com>
2026-08-10 23:45:54 -0300

    :sparkles: feat: continue and finish Services and add Ruler

--- AgendaCampo/Applications/Services/UsuarioService.cs
+++ AgendaCampo/Applications/Services/UsuarioService.cs
@@ -6,6 +6,8 @@ using System.Runtime.Intrinsics.Arm;
 using System.Security.Cryptography;
 using System.Text;
 
+using AgendaCampo.Applications.Validações;
+
 namespace RoyalGamess.Aplications.Services
 {
     public class UsuarioService
@@ -45,6 +47,93 @@ namespace RoyalGamess.Aplications.Services
             return lerUsuarioDTO;
         }
 
+
+        public lerUsuarioDTO BuscarPorID(Guid id)
+        {
+            Usuario usuarioDto = _rep.ObterPorId(id);
+            if(usuarioDto == null)
+                throw new DomainException("Usuario no encontrado");
+
+            return lerDTO(usuarioDto);
+
+        }
+
+        public lerUsuarioDTO buscarPorEmail(string email)
+        {
+            Usuario usuarioDTO = _rep.ObterPorEmail(email);
+            if (usuarioDTO == null)
+                throw new DomainException("Usuario nao encontrado");
+
+            return lerDTO(usuarioDTO);
+        }
+
+
+        public lerUsuarioDTO Adicionar(criarUsuarioDTO usuarioDTO)
+        {
+            Validacoes.validarEmail(usuarioDTO.email);
+            
+            Validacoes.validarNome(usuarioDTO.nome);
+
+            if (_rep.EmailExiste(usuarioDTO.email))
+                throw new DomainException("Email invalido");
+
+            Usuario usuario = new Usuario
+            {
+                nome = usuarioDTO.nome,
+                email = usuarioDTO.email,
+                senha = HashSenha_(usuarioDTO.senha),
+                statusUsuario = true
+            };
+            
+            _rep.Adicionar(usuario);
+            return lerDTO(usuario);
+        }
+
+        public lerUsuarioDTO Atualizar(Guid id, atualizarUsuarioDTO usuarioDTO)
+        {
+            Usuario usuarioBanco = _rep.ObterPorId(id);
+
+            if (usuarioBanco == null)
+                throw new DomainException("Usuario nao encontrado");
+            
+            Validacoes.validarEmail(usuarioDTO.email);
+            Validacoes.validarNome(usuarioDTO.nome);
+
+            if (usuarioDTO != null && usuarioBanco.usuarioID != id)
+                throw new DomainException("Usuario inexistente");
+
+            usuarioBanco.email = usuarioDTO.email;
+            usuarioBanco.nome = usuarioDTO.nome;
+            usuarioBanco.senha = HashSenha_(usuarioDTO.senha);
+            
+            _rep.Atualizar(usuarioBanco);
+            return lerDTO(usuarioBanco);
+        }
+
+        public void AtualizarSenha(Guid id, atualizarUsuarioDTO atualizarDTO)
+        {
+            
+            Usuario usuarioBanco = _rep.ObterPorId(id);
+
+            if (usuarioBanco == null)
+                throw new DomainException("Usuario nao encontrado");
+            usuarioBanco.senha = HashSenha_(atualizarDTO.senha);
+            
+            _rep.AtualizarSenha(id, usuarioBanco.senha);
+            // return lerDTO(atualizarDTO);
+
+        }
+
+
+        public void Remover(Guid id)
+        {
+            Usuario usuario = _rep.ObterPorId(id);
+            if (usuario == null)
+                throw new DomainException("Usuario nao encontrado");
+            
+            _rep.Remover(id);
+        }
+        
     }
 
 }
