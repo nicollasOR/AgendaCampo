ffc12cc8248baa2c506d94714a7ab5311f33e39c
nicollasOR <nicollas.oliveira.rufino30@gmail.com>
2026-08-10 23:47:07 -0300

    :sparkles: feat: add HTTP(GET{ID}, GET{Email}, POST, PUT, PATCH(senha), DELETE)

--- AgendaCampo/Controllers/UsuarioController.cs
+++ AgendaCampo/Controllers/UsuarioController.cs
@@ -1,4 +1,6 @@
+using AgendaCampo.Domains;
 using AgendaCampo.DTOs.UsuarioDTO;
+using AgendaCampo.Exceptions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using RoyalGamess.Aplications.Services;
@@ -29,6 +31,90 @@ namespace AgendaCampo.Controllers
             else
             {
                 return Ok(list);
+            }
+
+        }
+        
+        [HttpGet("{id}")]
+        public ActionResult<lerUsuarioDTO> ObterPorId(Guid id)
+        {
+            try
+            {
+                lerUsuarioDTO usuario = _service.BuscarPorID(id);
+                return Ok(usuario);
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+        [HttpGet("email/{email}")]
+        public ActionResult<lerUsuarioDTO> ObterPorEmail(string email)
+        {
+            try
+            {
+                lerUsuarioDTO usuario = _service.buscarPorEmail(email);
+                return Ok(usuario);
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+        [HttpPost]
+        public ActionResult<lerUsuarioDTO> Adicionar(criarUsuarioDTO criarDto)
+        {
+            try
+            {
+                lerUsuarioDTO lerDto = _service.Adicionar(criarDto);
+                return StatusCode(201, lerDto);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPut("{id}")]
+        public ActionResult<lerUsuarioDTO> Atualizar(Guid id, atualizarUsuarioDTO criarDto)
+        {
+            try
+            {
+                lerUsuarioDTO lerDto = _service.Atualizar(id, criarDto);
+                return Ok(lerDto);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPatch("{id}")]
+        public ActionResult<atualizarUsuarioDTO> AtualizarSenha(Guid id, atualizarUsuarioDTO atualizarDTO)
+        {
+            try
+            {
+                _service.AtualizarSenha(id, atualizarDTO);
+                return NoContent();
+
+            }
+            catch (DomainException e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+        
+        
+        [HttpDelete("{id}")]
+        public ActionResult Remover(Guid id)
+        {
+            try
+            {
+                _service.Remover(id);
+                return Ok();
+            }
+            catch (DomainException ex)
+            {
+                return NotFound(ex.Message);
             }
 
         }
