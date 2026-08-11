7a75f379778497bc854e157341bf52a45b88abd2
nicollasOR <nicollas.oliveira.rufino30@gmail.com>
2026-08-11 17:08:06 -0300

    :card_file_box: feat: add new columns on Evento (sql)

--- AgendaCampo_scheme.sql
+++ AgendaCampo_scheme.sql
@@ -1,103 +1,117 @@
 CREATE DATABASE AgendaCampo
 GO
-
 USE AgendaCampo
 CREATE TABLE [Usuario] (
-    [usuarioID] uniqueidentifier PRIMARY KEY NOT NULL default NEWID(),
-                        [nome] nvarchar(60) NOT NULL,
-                        [email] varchar(60) UNIQUE NOT NULL,
-                        [senha] varbinary(32) NOT NULL,
-                        [statusUsuario] bit default 1
+                           [usuarioID] uniqueidentifier PRIMARY KEY NOT NULL default NEWID(),
+                           [nome] nvarchar(60) NOT NULL,
+                           [email] varchar(60) UNIQUE NOT NULL,
+                           [senha] varbinary(32) NOT NULL,
+                           [statusUsuario] bit default 1
 )
-GO
 
+INSERT INTO Usuario(nome, email, senha)
+VALUES
+    ('teste', 'teste@gmail.com', HASHBYTES('SHA2_256', 'teste'))
+GO
+SELECT * FROM Usuario
 
 CREATE TABLE [Agendamento] (
-    [agendaID] int PRIMARY KEY NOT NULL,
-    [data] datetime2,
-    [empresaSede] nvarchar(50) NOT NULL,
-                            [usuarioID] uniqueidentifier NOT NULL,
-                            [statusAgenda] bit default 1 NOT NULL,
+                               [agendaID] int PRIMARY KEY NOT NULL,
+                               [data] datetime2,
+                               [empresaSede] nvarchar(50) NOT NULL,
+                               [usuarioID] uniqueidentifier NOT NULL,
+                               [statusAgenda] bit default 1 NOT NULL,
 
 
-                            CONSTRAINT FK_Agendamento_Usuario FOREIGN KEY(usuarioID) REFERENCES Usuario(usuarioID) ON DELETE CASCADE
+                               CONSTRAINT FK_Agendamento_Usuario FOREIGN KEY(usuarioID) REFERENCES Usuario(usuarioID) ON DELETE CASCADE
 )
 
 go
 
 CREATE TABLE [Endereco] (
-    [enderecoID] int PRIMARY KEY,
-    [logradouro] nvarchar(60) NOT NULL,
-                         [bairro] nvarchar(40) NOT NULL,
-                         [numero] int NOT NULL,
-                         [cep] nvarchar(8) not null
+                            [enderecoID] int PRIMARY KEY,
+                            [logradouro] nvarchar(60) NOT NULL,
+                            [bairro] nvarchar(40) NOT NULL,
+                            [numero] int NOT NULL,
+                            [cep] nvarchar(8) not null
 )
 GO
 
+-- CREATE TABLE StatusVisita(
+--     [statusVisitaID] INT PRIMARY KEY IDENTITY(1,1),
+--     [nomeStatus] NVARCHAR(30) NOT NULL
+-- )
+
 CREATE TABLE [Visita] (
-    [agendamentoID] int NOT NULL,
-    [enderecoID] int NOT NULL,
-    [titulo] nvarchar(50) NOT NULL,
-                       [descricao] nvarchar(max),
-                       [statusRealizado] bit default 0 NOT NULL,
+                          [visitaID] int primary key identity(1,1) not null,
+                          [agendamentoID] int NOT NULL,
+                          [enderecoID] int NOT NULL,
+                          --[statusVisitaID] int not null,
+                          [sedeVisitada] NVARCHAR(60) null,
+                          [titulo] nvarchar(50) NOT NULL,
+                          [descricao] nvarchar(max),
+                          [statusRealizado] bit default 0 NOT NULL,
+                          [dataInicio] DATETIME2 NOT NULL,
+                          [dataTermino] DATETIME2 NOT NULL,
 
 
-                       CONSTRAINT FK_Visita_AgendamentoID FOREIGN KEY(agendamentoID)
-                       REFERENCES Agendamento(agendaID) ON DELETE CASCADE,
 
-                       CONSTRAINT FK_Visita_Endereco FOREIGN KEY(enderecoID)
-                       REFERENCES Endereco(enderecoID) ON DELETE CASCADE
+                          CONSTRAINT FK_Visita_AgendamentoID FOREIGN KEY(agendamentoID)
+                              REFERENCES Agendamento(agendaID) ON DELETE CASCADE,
 
+                          CONSTRAINT FK_Visita_Endereco FOREIGN KEY(enderecoID)
+                              REFERENCES Endereco(enderecoID) ON DELETE CASCADE,
 
+                          --CONSTRAINT FK_Visita_statusVisita FOREIGN KEY(statusVisitaID)
+                             -- REFERENCES StatusVisita(statusVisitaID) ON DELETE CASCADE
 )
-
 
 
 CREATE TRIGGER trg_softDelete_Usuario
-ON Usuario
-INSTEAD OF DELETE
-AS
-BEGIN
-UPDATE usr SET statusUsuario = 0
-from Usuario usr
-INNER JOIN deleted d
-ON d.usuarioID = usr.usuarioID
-END
-GO
+    ON Usuario
+    INSTEAD OF DELETE
+    AS
+    BEGIN
+      UPDATE usr SET statusUsuario = 0
+        from Usuario usr
+            INNER JOIN deleted d
+            ON d.usuarioID = usr.usuarioID
+    END
+    GO
 
- CREATE TRIGGER trg_checagem_Agendamento
-     ON Agendamento
-     AFTER INSERT, UPDATE
-     AS
-     BEGIN
-     UPDATE Agnd
-         SET Agnd.statusAgenda = 0
-         FROM Agendamento Agnd
-             INNER JOIN inserted updtBit ON Agnd.agendaID = updtBit.agendaID
-                 WHERE updtBit.data > SYSDATETIME();
-     END
-     GO
+-- CREATE TRIGGER trg_checagem_Agendamento
+--     ON Agendamento
+--     AFTER INSERT, UPDATE
+--     AS
+--     BEGIN
+--     UPDATE Agnd
+--         SET Agnd.statusAgenda = 0
+--         FROM Agendamento Agnd
+--             INNER JOIN inserted updtBit ON Agnd.agendaID = updtBit.agendaID
+--                 WHERE updtBit.data > SYSDATETIME();
+--     END
+--     GO
+--
+--
+-- CREATE TRIGGER trg_checagem_Visita
+--     ON Visita
+--     AFTER INSERT, UPDATE
+--     AS
+-- BEGIN
+--     UPDATE vis
+--     SET vis.statusRealizado = 1
+--     FROM Visita vis
+--              INNER JOIN inserted updtBit ON vis.statusRealizado = updtBIT.statusRealizado
+--     WHERE updtBit.statusRealizado > SYSDATETIME();
+-- END
+-- GO
 
 
- --CREATE TRIGGER trg_checagem_Visita
- --    ON Visita
- --    AFTER INSERT, UPDATE
- --    AS
- --BEGIN
- --    UPDATE vis
- --    SET vis.statusRealizado = 1
- --    FROM Visita vis
- --             INNER JOIN inserted updtBit ON vis.statusRealizado = updtBIT.statusRealizado
- --    WHERE updtBit.statusRealizado > SYSDATETIME();
- --END
- --GO
-
-
- --CREATE TRIGGER trg_softDelete_Visita
- --    ON Visita
- --    INSTEAD OF DELETE
- --    AS
- --    BEGIN
- --        UPDATE vis set statusRealizado = 0
- --    end
+-- CREATE TRIGGER trg_softDelete_Visita
+--     ON Visita
+--     INSTEAD OF DELETE
+--     AS
+--     BEGIN
+--         UPDATE vis set statusRealizado = 0
+--     end
 
