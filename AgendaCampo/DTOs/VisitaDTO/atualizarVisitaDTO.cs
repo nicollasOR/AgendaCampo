25a38120da4d844f93dfedefbc74558487cc10fb
nicollasOR <nicollas.oliveira.rufino30@gmail.com>
2026-08-12 10:44:03 -0300

    :sparkles: feat: update DTOs

--- AgendaCampo/DTOs/VisitaDTO/atualizarVisitaDTO.cs
+++ AgendaCampo/DTOs/VisitaDTO/atualizarVisitaDTO.cs
@@ -16,25 +16,25 @@ public class atualizarVisitaDTO
     public bool? statusRealizado { get; set; }
 
     [Required(ErrorMessage = "um id de agendamento é obrigatório")]
-    public int? agendamentoId { get; set; }
+    public int agendamentoId { get; set; }
     
     [Required(ErrorMessage = "um id de endereco é obrigatório")]
-    public int? enderecoId { get; set; }
+    public int enderecoId { get; set; }
 
-    [Required(ErrorMessage = "Um nome do que e qual sede será visitada, é obrigatório")]
-    public string nomeSede { get; set; } = string.Empty;
+    //[Required(ErrorMessage = "Um nome do que e qual sede será visitada, é obrigatório")]
+    //public string nomeSede { get; set; } = string.Empty;
 
     [Required(ErrorMessage = "uma data inicial é obrigatória")]
-    public DateTime? dataInicio { get; set; } 
+    public DateTime dataInicio { get; set; } 
     [Required(ErrorMessage = "uma data final é obrigatória")]
-    public DateTime? dataTermino { get; set; }
+    public DateTime dataTermino { get; set; }
     
     
 }
 
 public class lerVisitaDTO
 {
- 
+    public int visitaID { get; set; }
     [Required(ErrorMessage = "O nome do evento é obrigatório")]
     [MaxLength(70, ErrorMessage = "É permitido no maximo 50 caracteres")]
     public string nomeEvento { get; set; } = null!;
