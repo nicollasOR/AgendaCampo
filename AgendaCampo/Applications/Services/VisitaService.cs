8d6155507ed62ab10ed6c5c0ab9ecabb1826e7fc
nicollasOR <nicollas.oliveira.rufino30@gmail.com>
2026-08-12 10:45:46 -0300

    :sparkles: feat: add more methods

--- AgendaCampo/Applications/Services/VisitaService.cs
+++ AgendaCampo/Applications/Services/VisitaService.cs
@@ -20,6 +20,7 @@ public class VisitaService
     {
         return new lerVisitaDTO
         {
+            visitaID = visita.visitaID,
             agendamentoId = visita.agendamentoID,
             dataInicio = visita.dataInicio,
             dataTermino = visita.dataTermino,
@@ -41,6 +42,45 @@ public class VisitaService
         return lerVisitas;
     }
 
+    public lerVisitaDTO buscarPorId(int id)
+    {
+        Visita visitaBanco = _rep.BuscarPorId(id);
+        if (visitaBanco == null)
+            throw new DomainException("Visita não encontrada");
+
+        return lerDTO(visitaBanco);
+
+    }
+
+    public lerVisitaDTO buscarPorAgendamento(DateTime date)
+    {
+        Visita visitaBanco = _rep.BuscarPorAgendamento(date);
+        if (visitaBanco == null)
+            throw new DomainException("Visita não encontrada");
+
+        return lerDTO(visitaBanco);
+    }
+
+    public lerVisitaDTO buscarPorEndereco(string logradouro)
+    {
+        //if (_rep.enderecoExiste(logradouro))
+        //    throw new DomainException("");
+        Visita visitaBanco = _rep.BuscarPorEndereco(logradouro);
+        if (visitaBanco == null)
+            throw new DomainException("Visita não encontrada");
+
+        return lerDTO(visitaBanco);
+    }
+
+    public lerVisitaDTO buscarPorTitulo(string titulo)
+    {
+        Visita visitaBanco = _rep.BuscarPorTitulo(titulo);
+        if (visitaBanco == null)
+            throw new DomainException("Visita não encontrada");
+
+        return lerDTO(visitaBanco);
+    }
+
     public lerVisitaDTO Adicionar(criarVisitaDTO criarVisitaDto)
     {
         var agndBanco = _agndRep.buscarPorId(criarVisitaDto.agendamentoId);
@@ -49,7 +89,7 @@ public class VisitaService
 
         if (agndBanco.data != criarVisitaDto.dataInicio)
             throw new DomainException("A visita deve ter a mesma data, hora de inicio do agendamento");
-        
+      
       // falta adicionar o endereco de validação.
 
       Visita visita = new Visita
@@ -68,5 +108,62 @@ public class VisitaService
       return lerDTO(visita);
     }
     
-    
+    public lerVisitaDTO Atualizar(int id, atualizarVisitaDTO atualizarDTO)
+    {
+        Visita visitaBanco = _rep.BuscarPorId(id);
+        if (visitaBanco == null)
+            throw new DomainException("Visita não encontrada");
+
+        if (!(_rep.agendamentoExiste(visitaBanco.agendamentoID)) || !(_rep.enderecoExiste(visitaBanco.enderecoID)))
+            throw new DomainException("Agendamento e/ou endereço não encontrado");
+
+        visitaBanco.titulo = atualizarDTO.nomeEvento;
+        visitaBanco.descricao = atualizarDTO.descricao;
+        visitaBanco.dataTermino = atualizarDTO.dataTermino;
+        visitaBanco.dataInicio = atualizarDTO.dataInicio;
+        visitaBanco.agendamentoID = visitaBanco.agendamentoID;
+        visitaBanco.enderecoID = visitaBanco.enderecoID;
+
+        _rep.Atualizar(visitaBanco, visitaBanco.agendamentoID, visitaBanco.enderecoID);
+
+        return lerDTO(visitaBanco);
+        //visitaBanco
+        //if(!(_rep.enderecoExiste))
+
+    }
+
+
+    public void atualizarEndereco(int id, atualizarVisitaDTO atlDTO)
+    {
+        Visita visitaBanco = _rep.BuscarPorId(id);
+
+        if (visitaBanco == null)
+            throw new DomainException("Visita não encontrada");
+
+        visitaBanco.enderecoID = atlDTO.enderecoId;
+
+        _rep.atualizarEndereco(id, visitaBanco.enderecoID);
+    }
+
+    public void atualizarAgendamento(int id, atualizarVisitaDTO atlDTO)
+    {
+        Visita visitaBanco = _rep.BuscarPorId(id);
+
+        if (visitaBanco == null)
+            throw new DomainException("Visita não encontrada");
+
+        visitaBanco.agendamentoID = atlDTO.agendamentoId;
+
+        _rep.atualizarEndereco(id, visitaBanco.agendamentoID);
+        //return lerDTO(atlDTO);
+    }
+
+    public void Remover(int id)
+    {
+        Visita visitaBanco = _rep.BuscarPorId(id);
+        if (visitaBanco == null)
+            throw new DomainException("Visita não encontrada");
+        _rep.Remover(id);
+
+    }
 }
