cf7d38aed39a91887a94f69e458fe3b3f9b51e75
Mayara Almeida <mayara.s.almeida8@aluno.senai.br>
2026-09-06 10:45:12 -0300

    :wrench: feat: ajuste da .env

--- AgendaCampoReactNative/src/service/api.ts
+++ AgendaCampoReactNative/src/service/api.ts
@@ -3,7 +3,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const TOKEN_KEY = "@agenda_campo:token";
 
-const BASE_URL = "http://10.0.2.2:5100/api/";
+const BASE_URL = "http://192.168.0.22:5100/api/";
 
 export const api = axios.create({
   baseURL: BASE_URL,
